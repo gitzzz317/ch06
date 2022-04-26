ec73f92911ca0720c401b7e62baf0bcfa1b87fc1
hzw001 <2832572746@qq.com>
2022-04-26 16:57:18 +0800

    添加拦截器

---
+++ src/com/zhbit/notice/web/interceptors/loginInterceptor.java
@@ -0,0 +1,36 @@
+package com.zhbit.notice.web.interceptors;
+
+import com.zhbit.notice.pojo.User;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Component
+public class loginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("LOGINED_USER");
+        if(user==null){
+            //没有登录
+            response.sendRedirect(request.getContextPath()+"/login.jsp");
+            return false;
+        }
+        //放行  访问目标资源
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
